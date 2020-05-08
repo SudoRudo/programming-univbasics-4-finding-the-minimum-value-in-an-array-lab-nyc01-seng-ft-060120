aaee2b6bfad7a459bed37b6f36638f47fbc0247a
Rudy Guerrero <r.guerreronyc@gmail.com>
2020-05-08 08:14:23 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,4 +1,11 @@
 def find_min_value(array)
-  i = 0
-  while  
+  i = 1
+  min = array[0]
+  while i < array.length do
+    if array[i] < array[i - 1]
+      min = array[i]
+    end
+    i += 1
+  end
+  return min
 end
